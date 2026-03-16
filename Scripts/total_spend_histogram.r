33d6060c6ac089ddece18115859947173b58b630
Adam Dyrda <a.dyrda102@gmail.com>
2026-03-16 22:00:31 +0100

    Changes in size of the graph to better fit the raport

--- Scripts/total_spend_histogram.r
+++ Scripts/total_spend_histogram.r
@@ -6,7 +6,7 @@ my_data <- read_csv("./starbucks_customer_ordering_patterns.csv")
 
 chart_data <- my_data$total_spend
 
-pdf("Resources/Charts/total_spend_histogram.pdf", width = 5, height = 5)
+pdf("Resources/Charts/total_spend_histogram.pdf", width = 5, height = 4)
 
 hist(chart_data, col = "#007BA8", border = "white", main = "", xlab = "value of the order in USD", breaks = 20, xlim = c(0, 40))
 
