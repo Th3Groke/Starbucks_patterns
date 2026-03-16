d6db87486a350bd31518ee77e1bbce664fd57f48
Adam Dyrda <a.dyrda102@gmail.com>
2026-03-16 20:42:18 +0100

    changes in the order_channel feature description and change in size of the pie chart

--- Scripts/order_channel_pie_chart.r
+++ Scripts/order_channel_pie_chart.r
@@ -7,7 +7,7 @@ my_data <- read_csv("./starbucks_customer_ordering_patterns.csv")
 chart_data <- table(my_data$order_channel)
 
 colors <- c("skyblue", "orange", "chartreuse", "tomato")
-pdf("Resources/Charts/order_channel_pie_chart.pdf", width = 7,height = 7)
+pdf("Resources/Charts/order_channel_pie_chart.pdf", width = 5,height = 4)
 
 pie(chart_data,
     labels = paste0(names(chart_data), " (", chart_data, ")"),
