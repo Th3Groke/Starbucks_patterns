aca8e7332b089fa7afb5f643bffc10f93227a05b
pyrxk <mp318296@student.polsl.pl>
2026-03-19 13:47:00 +0100

    Added percentages to the order channel pie chart

--- Scripts/Charts/order_channel_pie_chart.r
+++ Scripts/Charts/order_channel_pie_chart.r
@@ -10,7 +10,7 @@ colors <- c("skyblue", "orange", "chartreuse", "tomato")
 pdf("Resources/Charts/order_channel_pie_chart.pdf", width = 5,height = 4)
 
 pie(chart_data,
-    labels = paste0(names(chart_data), " (", chart_data, ")"),
+    labels = paste0(names(chart_data), " (", round(chart_data/nrow(my_data)*100, 1), "%)"),
     col = colors)
 
 dev.off()
