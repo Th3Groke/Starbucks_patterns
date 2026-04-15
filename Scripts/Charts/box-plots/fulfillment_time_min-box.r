b6ad3aba4e8f0d57b1969c070cc27db721ba7afe
Adam Dyrda <130991488+Th3Groke@users.noreply.github.com>
2026-04-15 09:21:56 +0200

    finished box-plots (#30)

---
+++ Scripts/Charts/box-plots/fulfillment_time_min-box.r
@@ -0,0 +1,11 @@
+library(readr)
+
+data <- read_csv("starbucks_customer_ordering_patterns.csv")
+
+fulfillment <- data$fulfillment_time_min
+
+pdf("Resources/Charts/box-plots/fulfillemnt_time_min-box.pdf", width = 6)
+
+boxplot(fulfillment, col = c("#74dab6"),ylab="time for the order to be completed (minutes)")
+
+dev.off()
---
+++ Scripts/Charts/box-plots/total_spend-box.r
@@ -0,0 +1,11 @@
+library(readr)
+
+data <- read_csv("starbucks_customer_ordering_patterns.csv")
+
+total_spend <- data$total_spend
+
+pdf("Resources/Charts/box-plots/total_spend-box.pdf",width = 5)
+
+boxplot(total_spend, col = c("#669c3e"),
+        ylab = "Amount spent (USD)")
+dev.off()
