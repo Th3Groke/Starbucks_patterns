7982f8d9154e90b484e671c76791ad202d355141
Adam Dyrda <130991488+Th3Groke@users.noreply.github.com>
2026-05-29 14:24:20 +0200

    72 final feedback changes (#74)
    * references to Tables, mode value rounding
    
    * frequency in tab 2.1 and 2.2, proper values in tab 2.1
    
    * fixed the logical contradictions in 3.1 and 3.2
    
    * explained the discrepancy between the box-plots and the test
    
    * added disclaimer to the satisfaction box-plot and similarity
    
    * added .r to some r scripts that were missing extention

--- Scripts/descriptive/customer_age_group.r
+++ Scripts/descriptive/customer_age_group.r
@@ -5,10 +5,12 @@ dataset <- read.csv("starbucks_customer_ordering_patterns.csv")
 my_data <- table(dataset$customer_age_group)
 
 mode_age_group <- names(which.max(my_data))
+frequency <- (max(my_data))
 cardinality <- length(names(my_data))
 
 
 
 
 print(paste0("mode: ", mode_age_group))
+print(paste0("mode frequency: ", frequency))
 print(paste0("cardinality: ", cardinality))
